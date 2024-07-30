bb469272506c97d649dd00903a113d16f7b78d18
Vaidik Dubey <vaidik50000@gmail.com>
2024-07-30 12:22:10 +0530

    Update handler.py

--- ls/handler.py
+++ ls/handler.py
@@ -1,11 +1,20 @@
 import json
 
-
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!",
+    allowed_origins = 'http://localhost:3000,https://www.luxswipe.in'
+    origin = event.get('headers', {}).get('origin', '')
+    
+    if origin in allowed_origins.split(','):
+        access_control_allow_origin = origin
+    else:
+        access_control_allow_origin = allowed_origins.split(',')[0]  # Default to the first origin
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': access_control_allow_origin,
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps('Hello from Lambda!')
     }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
-    return response
